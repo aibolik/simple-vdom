f83c1c45725df902444d6819741f6c14c7eeffdf
Aibol Kussain <aibolikdev@gmail.com>
2018-06-23 11:10:49 +0300

    feat: create simple emojis list app with hyperscript

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
 import h from './my-hyperscript.js';
 
-const App = () => {
-  return h(
-    'h1',
-    null,
-    'Hello, vDOM!'
+const App = (props) => {
+  const { list } = props;
+
+  return h('div', { class: 'app' },
+    h('h1', null, 'Simple vDOM'),
+    h(
+      'ul', null,
+      ...list.map(item => h('li', null, item))
+    )
   );
 };
 
-console.log('App', App());
+let currentApp;
+const render = (state) => {
+  const newApp = App(state);
+  currentApp 
+    ? document.body.replaceChild(newApp, currentApp)
+    : document.body.appendChild(newApp);
+  
+  currentApp = newApp;
+};
+
+const state = {
+  list: [
+    '🕺', '💃', '😀', '🙋‍', '💼',
+    '🕶️️', '👏', '🤳', '🕵️', '👩‍🔧'
+  ]
+};
+
+render(state);
